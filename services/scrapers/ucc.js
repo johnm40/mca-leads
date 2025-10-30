be99fe6fef52942bf9f857e7a4312de53032bef5
johnm40 <johnm@forwardfundinggroup.com>
2025-10-30 14:33:58 -0400

    Create ucc.js

---
+++ services/scrapers/ucc.js
@@ -0,0 +1,11 @@
+export async function fetchUCC(page, limit) {
+  return [
+    {
+      business: "World Market",
+      address: "3532 S Jefferson St, Falls Church, VA",
+      revenue: "$6,240,000",
+      owner: "N/A",
+      source: "UCC",
+    },
+  ];
+}
